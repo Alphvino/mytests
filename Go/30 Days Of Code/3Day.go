f9a7ce8df39fb0df9229c6cbf06a304fbb3f94ff
Alphvino <49525225+Alphvino@users.noreply.github.com>
2021-07-18 19:10:24 +0200

    Create 3Day.go

---
+++ Go/30 Days Of Code/3Day.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var mealprice, tipercent, taxpercent float64
+	fmt.Scan(&mealprice, &tipercent, &taxpercent)
+	tip := (tipercent / 100) * mealprice
+	tax := (taxpercent / 100) * mealprice
+	total := mealprice + tip + tax
+	fmt.Println(math.Round(total))
+}
